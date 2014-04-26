cb889e83fc3f2796dd4c03db1331ca7a524e9af2
Jenny Allar <JennyAllar@gmail.com>
2014-04-26 13:56:57 -0600

    User can edit cat attributes.

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -14,4 +14,18 @@ feature 'Homepage' do
 
     expect(page).to have_content 'Siamese'
   end
+
+  scenario 'can update cat' do
+    visit '/cats/new'
+    fill_in 'name', with: 'Tabular'
+    fill_in 'color', with: 'Pink'
+    fill_in 'kittens', with: '4'
+    click_on 'Add Cat'
+    click_on 'Tabular'
+    fill_in 'name', with: 'Lola'
+    click_on 'Update Cat'
+    expect(page).to have_content('Lola')
+    expect(page).to_not have_content('Tabular')
+  end
+
 end
